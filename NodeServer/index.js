d8b142d50df3fc55f5f244d9baf297c23ace4c26
harsh082ip <109286547+harsh082ip@users.noreply.github.com>
2023-05-16 12:40:44 +0530

    initial commit

---
+++ NodeServer/index.js
@@ -0,0 +1,32 @@
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const io = new Server({
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
+});
+
+const users = {};
+
+io.on("connection", (socket) => {
+  socket.on("new-user-joined", (name) => {
+    console.log("new user", name);
+    users[socket.id] = name;
+    socket.broadcast.emit("user-joined", name);
+  });
+
+  socket.on("send", (message) => {
+    socket.broadcast.emit("receive", { message: message, name: users[socket.id] });
+  });
+
+  socket.on("disconnect", (message) => {
+    socket.broadcast.emit('left', users[socket.id]);  
+    delete users[socket.id];
+  });
+});
+
+io.listen(8000);
